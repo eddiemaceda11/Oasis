a479b6e618ddedbe1d75a13e62e6ced6bb87abc6
eddiemaceda11 <eddiemaceda11@gmail.com>
2024-07-12 17:15:02 -0400

    Update tremor css to light mode only

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -17,6 +17,7 @@
 //import defaultTheme from 'tailwindcss/defaultTheme';
 
 module.exports = {
+  darkMode: 'class',
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
