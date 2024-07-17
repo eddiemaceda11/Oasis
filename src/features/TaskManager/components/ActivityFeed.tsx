aafb0275af88a68a01a51d3c8fe88849ea474d6f
eddiemaceda11 <eddiemaceda11@gmail.com>
2024-07-17 16:30:58 -0400

    Add logic to show message if the activity feed is empty

--- src/features/TaskManager/components/ActivityFeed.tsx
+++ src/features/TaskManager/components/ActivityFeed.tsx
@@ -1,7 +1,6 @@
 import { useSelector } from "react-redux";
 import { type RootState } from "../../../store";
 
-
 const ActivityFeed = () => {
   const activities = useSelector((state: RootState) => state.tasks.activities);
   console.log("activities", activities);
@@ -10,25 +9,23 @@ const ActivityFeed = () => {
     <div className=" border rounded-lg bg-slate-100 h-[30rem] w-[34rem] overflow-hidden">
       <p className="bg-slate-100 pl-3 py-1 font-medium">Activity Feed</p>
       <div className="h-full bg-white">
-         {activities.map((activity) => (
-        <div key={activity.text} className="p-3">
-          <div className="flex relative">
-            <i className="flex items-center justify-center fa-solid fa-user absolute text-xl h-10 w-10 bg-slate-100 rounded-[50%] text-slate-400"></i>
-            <p className="font-medium text-sm ml-14">
-              {activity.name} updated this task
-            </p>
-            <p className="font-medium text-xs ml-auto text-slate-500">
-              {activity.timestamp}
-            </p>
-            <></>
+        {activities.length < 1 && <p className="ml-3 pt-3 italic">No activies logged...</p>}
+        {activities.map((activity) => (
+          <div key={activity.text} className="p-3">
+            <div className="flex relative">
+              <i className="flex items-center justify-center fa-solid fa-user absolute text-xl h-10 w-10 bg-slate-100 rounded-[50%] text-slate-400"></i>
+              <p className="font-medium text-sm ml-14">
+                {activity.name} updated this task
+              </p>
+              <p className="font-medium text-xs ml-auto text-slate-500">
+                {activity.timestamp}
+              </p>
+              <></>
+            </div>
+            <p className="text-xs w-[18rem] ml-14">"{activity.text}"</p>
           </div>
-          <p className="text-xs w-[18rem] ml-14">
-            "{activity.text}"
-          </p>
-        </div>
-      ))}
+        ))}
       </div>
-     
     </div>
   );
 };
