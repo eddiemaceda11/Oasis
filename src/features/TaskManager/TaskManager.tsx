2d18516d8f97417b1e0bf5604a15e19419421f33
eddiemaceda11 <eddiemaceda11@gmail.com>
2024-07-17 16:29:54 -0400

    Refactor code and Create TaskDetails component

--- src/features/TaskManager/TaskManager.tsx
+++ src/features/TaskManager/TaskManager.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { addActivity } from "./taskManagerSlice";
+import { formatCurrentDateTime } from "@/hooks/formatDateTime";
 
 import ActivityFeed from "./components/ActivityFeed";
 
@@ -16,7 +17,13 @@ const Tasks = () => {
 
   return (
     <section className="flex justify-between items-start m-auto md:w-[48rem] lg:bg-white lg:w-[61rem] xl:w-[80rem] h-[72rem] mb-4">
-      <QuickActions modalHidden={modalHidden} setModalHidden={setModalHidden} />
+      <div>
+        <QuickActions
+          modalHidden={modalHidden}
+          setModalHidden={setModalHidden}
+        />
+        <TaskDetails></TaskDetails>
+      </div>
       {modalHidden ? (
         ""
       ) : (
@@ -60,6 +67,38 @@ const QuickActions = ({ modalHidden, setModalHidden }: ModalProps) => {
           <i className="fa-regular fa-calendar-check mr-2"></i>
           <span>Complete Task</span>
         </button>
+      </div>
+    </div>
+  );
+};
+
+const TaskDetails = () => {
+  return (
+    <div className="flex justify-between items-center h-32 w-[45rem] rounded-lg mt-4">
+      <div className="border w-[8.3rem] h-32 rounded-lg flex flex-col items-center justify-center gap-1.5">
+        <i className="flex items-center justify-center fa-solid fa-user text-xl h-10 w-10 bg-slate-100 rounded-[50%] text-slate-400"></i>
+        <p className="font-medium text-sm">Created At</p>
+        <span className="text-xs">{formatCurrentDateTime()}</span>
+      </div>
+      <div className="border w-[8.3rem] h-32 rounded-lg flex flex-col items-center justify-center gap-1.5">
+        <i className="flex items-center justify-center fa-solid fa-user text-xl h-10 w-10 bg-slate-100 rounded-[50%] text-slate-400"></i>
+        <p className="font-medium text-sm">Tenant</p>
+        <span className="text-xs">John Doe</span>
+      </div>
+      <div className="border w-[8.3rem] h-32 rounded-lg flex flex-col items-center justify-center gap-1.5">
+        <i className="flex items-center justify-center fa-solid fa-user text-xl h-10 w-10 bg-slate-100 rounded-[50%] text-slate-400"></i>
+        <p className="font-medium text-sm">Unit #</p>
+        <span className="text-xs">34</span>
+      </div>
+      <div className="border w-[8.3rem] h-32 rounded-lg flex flex-col items-center justify-center gap-1.5">
+        <i className="flex items-center justify-center fa-solid fa-user text-xl h-10 w-10 bg-slate-100 rounded-[50%] text-slate-400"></i>
+        <p className="font-medium text-sm">Due Date</p>
+        <span className="text-xs">{formatCurrentDateTime()}</span>
+      </div>
+      <div className="border w-[8.3rem] h-32 rounded-lg flex flex-col items-center justify-center gap-1.5">
+        <i className="flex items-center justify-center fa-solid fa-user text-xl h-10 w-10 bg-slate-100 rounded-[50%] text-slate-400"></i>
+        <p className="font-medium text-sm">Status</p>
+        <span className="text-xs">In Progress</span>
       </div>
     </div>
   );
