6ce476c8fe2b8f7054ddea46d26f377ccc842aac
eddiemaceda11 <eddiemaceda11@gmail.com>
2024-07-19 16:59:01 -0400

    Code refactor

--- src/features/TaskManager/TaskManager.tsx
+++ src/features/TaskManager/TaskManager.tsx
@@ -115,9 +115,9 @@ const TaskDescription = () => {
           <p className="italic text-xs max-w-[20rem] ml-6">"I arrived home last night and the AC stopped working. Please send someone out to troubleshoot or fix it."</p>
         </div>
         <div className="ml-32">
-        <p className="text-medium py-3">Permission To Enter: <span className="text-green-500"><i className="fa-solid fa-circle-check"></i> Yes</span></p>
-        <p className="text-medium py-1">Phone: <span><i className="fa-solid fa-phone-volume"></i> 845-412-7892</span></p>
-        <p className="text-medium py-3">Priority: <span className="text-orange-300"><i className="fa-solid fa-circle-check"></i> Medium</span></p>
+          <p className="text-medium py-3">Permission To Enter: <span className="text-green-500"><i className="fa-solid fa-circle-check"></i> Yes</span></p>
+          <p className="text-medium py-1">Phone: <span><i className="fa-solid fa-phone-volume"></i> 845-412-7892</span></p>
+          <p className="text-medium py-3">Priority: <span className="text-orange-300"><i className="fa-solid fa-circle-check"></i> Medium</span></p>
         </div>
       </div>
     </div>
