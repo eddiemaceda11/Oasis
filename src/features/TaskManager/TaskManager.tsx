4ae5aa8f8eef11516ded42f0b5cb647467e75f6f
eddiemaceda11 <eddiemaceda11@gmail.com>
2024-07-19 17:10:44 -0400

    Extract the TaskDescription component into its own file

--- src/features/TaskManager/TaskManager.tsx
+++ src/features/TaskManager/TaskManager.tsx
@@ -4,6 +4,7 @@ import { addActivity } from "./taskManagerSlice";
 import { formatCurrentDateTime } from "@/hooks/formatDateTime";
 
 import QuickActions from "./components/QuickActions"
+import TaskDescription from "./components/TaskDescription";
 import ActivityFeed from "./components/ActivityFeed";
 
 // TODO - seperate necessary components into their own files
@@ -67,25 +68,6 @@ const TaskDetails = () => {
     </div>
   );
 };
-
-const TaskDescription = () => {
-  return (
-    <div className="border h-[10.95rem] w-[45rem] rounded-lg mt-4">
-      <p className="bg-slate-100 pl-4 font-medium py-1">Description</p>
-      <div className="flex">
-        <div>
-          <p className="pl-5 text-medium py-3">Tenant submitted a ticket:</p>
-          <p className="italic text-xs max-w-[20rem] ml-6">"I arrived home last night and the AC stopped working. Please send someone out to troubleshoot or fix it."</p>
-        </div>
-        <div className="ml-32">
-          <p className="text-medium py-3">Permission To Enter: <span className="text-green-500"><i className="fa-solid fa-circle-check"></i> Yes</span></p>
-          <p className="text-medium py-1">Phone: <span><i className="fa-solid fa-phone-volume"></i> 845-412-7892</span></p>
-          <p className="text-medium py-3">Priority: <span className="text-orange-300"><i className="fa-solid fa-circle-check"></i> Medium</span></p>
-        </div>
-      </div>
-    </div>
-  )
-}
 
 const TaskModal = ({ modalHidden, setModalHidden }: ModalProps) => {
   const [input, setInput] = useState("");
---
+++ src/features/TaskManager/components/TaskDescription.tsx
@@ -0,0 +1,20 @@
+const TaskDescription = () => {
+    return (
+      <div className="border h-[10.95rem] w-[45rem] rounded-lg mt-4">
+        <p className="bg-slate-100 pl-4 font-medium py-1">Description</p>
+        <div className="flex">
+          <div>
+            <p className="pl-5 text-medium py-3">Tenant submitted a ticket:</p>
+            <p className="italic text-xs max-w-[20rem] ml-6">"I arrived home last night and the AC stopped working. Please send someone out to troubleshoot or fix it."</p>
+          </div>
+          <div className="ml-32">
+            <p className="text-medium py-3">Permission To Enter: <span className="text-green-500"><i className="fa-solid fa-circle-check"></i> Yes</span></p>
+            <p className="text-medium py-1">Phone: <span><i className="fa-solid fa-phone-volume"></i> 845-412-7892</span></p>
+            <p className="text-medium py-3">Priority: <span className="text-orange-300"><i className="fa-solid fa-circle-check"></i> Medium</span></p>
+          </div>
+        </div>
+      </div>
+    )
+  }
+
+export default TaskDescription;
