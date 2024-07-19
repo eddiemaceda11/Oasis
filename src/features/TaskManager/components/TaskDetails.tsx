831fa8f787631cbc10835db8b33c0c5a937614e3
eddiemaceda11 <eddiemaceda11@gmail.com>
2024-07-19 17:12:35 -0400

    Extract the TaskDetails component into a separate file

---
+++ src/features/TaskManager/components/TaskDetails.tsx
@@ -0,0 +1,35 @@
+import { formatCurrentDateTime } from "@/hooks/formatDateTime";
+
+const TaskDetails = () => {
+    return (
+      <div className="flex justify-between items-center h-32 w-[45rem] rounded-lg mt-4">
+        <div className="border w-[8.3rem] h-32 rounded-lg flex flex-col items-center justify-center gap-1.5">
+          <i className="flex items-center justify-center fa-solid fa-user text-xl h-10 w-10 bg-slate-100 rounded-[50%] text-slate-400"></i>
+          <p className="font-medium text-sm">Created At</p>
+          <span className="text-xs">{formatCurrentDateTime()}</span>
+        </div>
+        <div className="border w-[8.3rem] h-32 rounded-lg flex flex-col items-center justify-center gap-1.5">
+          <i className="flex items-center justify-center fa-solid fa-user text-xl h-10 w-10 bg-slate-100 rounded-[50%] text-slate-400"></i>
+          <p className="font-medium text-sm">Tenant</p>
+          <span className="text-xs">John Doe</span>
+        </div>
+        <div className="border w-[8.3rem] h-32 rounded-lg flex flex-col items-center justify-center gap-1.5">
+          <i className="flex items-center justify-center fa-solid fa-user text-xl h-10 w-10 bg-slate-100 rounded-[50%] text-slate-400"></i>
+          <p className="font-medium text-sm">Unit #</p>
+          <span className="text-xs">34</span>
+        </div>
+        <div className="border w-[8.3rem] h-32 rounded-lg flex flex-col items-center justify-center gap-1.5">
+          <i className="flex items-center justify-center fa-solid fa-user text-xl h-10 w-10 bg-slate-100 rounded-[50%] text-slate-400"></i>
+          <p className="font-medium text-sm">Due Date</p>
+          <span className="text-xs">{formatCurrentDateTime()}</span>
+        </div>
+        <div className="border w-[8.3rem] h-32 rounded-lg flex flex-col items-center justify-center gap-1.5">
+          <i className="flex items-center justify-center fa-solid fa-user text-xl h-10 w-10 bg-slate-100 rounded-[50%] text-slate-400"></i>
+          <p className="font-medium text-sm">Status</p>
+          <span className="text-xs">In Progress</span>
+        </div>
+      </div>
+    );
+  };
+
+  export default TaskDetails
