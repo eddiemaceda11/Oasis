346fde404ccb9a3c6876d4f34889f2bbe193ed3b
Eddie Maceda <eddiemaceda11@gmail.com>
2024-07-29 15:59:09 -0400

    Install react-router and react-router-dom

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import React from "react";
 import "./index.css";
 import App from "./app/App";
 
+import { createBrowserRouter, RouterProvider, Route, Link } from "react-router-dom";
+
 // add store and provider
 import { store } from "./store";
 import { Provider } from "react-redux";
