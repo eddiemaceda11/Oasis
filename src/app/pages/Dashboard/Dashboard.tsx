8d71cb10dfbc5063b64bdd33e9f54834dab60617
Eddie Maceda <eddiemaceda11@gmail.com>
2024-08-12 22:32:58 -0400

    Add h-full to component

--- src/app/pages/Dashboard/Dashboard.tsx
+++ src/app/pages/Dashboard/Dashboard.tsx
@@ -3,10 +3,10 @@ import { Outlet } from "react-router";
 
 const DashboardPage = () => {
   return (
-    <div className="h-1900 w-full max-w-[1950px] m-auto">
+    <div className="h-full w-full max-w-[1950px] m-auto">
       <div className="flex flex-1 h-full justify-between overflow-scroll">
         <Sidebar />
-        <div className="w-full flex flex-col ">
+        <div className="h-full w-full flex flex-col ">
           <Outlet />
         </div>
       </div>
