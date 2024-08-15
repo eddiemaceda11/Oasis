5171fc032604a548886ed6c0d57cf83089adb0ea
Eddie Maceda <eddiemaceda11@gmail.com>
2024-08-14 23:19:17 -0400

    Update import Paths, remove Tasks component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -9,15 +9,15 @@ import DashboardPage from "./pages/Dashboard/Dashboard";
 import Header from "../components/layouts/Header";
 
 // Features
-import Rentals from "../features/Rentals/Rentals";
-import Booking from "../features/Booking/Booking";
+import Rentals from "../features/Home/Rentals/Rentals";
+import Booking from "../features/Home/Booking/Booking";
 
 // Table
 import { TableHero } from "../components/ui/Table";
 // import TableModal from '../components/ui/TableModal';
 
 // Tasks manager
-import Tasks from "../features/TaskManager/TaskManager";
+import Tasks from "../features/Dashboard/TaskManager/TaskManager";
 
 function App() {
   return (
@@ -25,9 +25,6 @@ function App() {
       <div className="m-auto h-full overflow-x-hidden">
         <Header />
         <h1 className="m-auto text-4xl font-semibold text-center w-[45rem] h-28">Customize Property Management Ensuring Your Peace of Mind</h1>
-
-        <Tasks />
-
         <Booking />
         <Rentals />
         <TableHero />
