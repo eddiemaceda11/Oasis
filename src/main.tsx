fd8bb5bb59f154c1082731e55070fb9fcf8c7f2c
Eddie Maceda <eddiemaceda11@gmail.com>
2024-08-16 06:11:44 -0400

    Fix import

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,11 @@ import "./index.css";
 // add store and provider
 import { store } from "./store";
 import { Provider } from "react-redux";
-import { create } from "domain";
 
 import App from "./app/App";
 import DashboardPage from "./app/pages/Dashboard/Dashboard";
 import Tasks from "./features/Dashboard/TaskManager/TaskManager";
-import TenantsTable from "@/features/Dashboard/Tenants/Tenants";
+import TenantsTable from "./features/Dashboard/Tenants/Tenants";
 import DashboardHome from "./features/Dashboard/Home/Home";
 
 import { createBrowserRouter, RouterProvider, Route, Link } from "react-router-dom";
