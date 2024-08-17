888a6568148d28d3fb481df179f1c771379ba104
Eddie Maceda <eddiemaceda11@gmail.com>
2024-08-17 18:37:45 -0400

    Adjust conic gradient for CircularProgress

--- src/components/ui/CircularProgress.tsx
+++ src/components/ui/CircularProgress.tsx
@@ -2,14 +2,14 @@ const CircularProgress = ({ progress }: { progress: number }) => {
   return (
     <div className="relative flex items-center justify-center h-24 w-24">
       <div
-        className="absolute w-full h-full rounded-full"
+        className="absolute w-full h-full"
         style={{
-          background: `conic-gradient(#3bd1f6 ${progress}%, #e5e7eb ${progress}%)`,
+          background: `conic-gradient(#3bd1f6 ${progress}%, #e5e7eb ${progress}% 85%, red 85%)`,
           borderRadius: "50%",
           clipPath: "circle(50% at 50% 50%)",
         }}
       />
-      <div className="absolute flex items-center justify-center h-16 w-16 bg-white rounded-[50%]">{progress}%</div>
+      <div className=" absolute flex items-center justify-center h-16 w-16 bg-white rounded-[50%]">{progress}%</div>
     </div>
   );
 };
