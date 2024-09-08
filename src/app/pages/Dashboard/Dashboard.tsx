0d0599f84e96812502e49e362193eb04dc3d8d98
Eddie Maceda <eddiemaceda11@gmail.com>
2024-09-08 17:05:20 -0400

    Modified styles

--- src/app/pages/Dashboard/Dashboard.tsx
+++ src/app/pages/Dashboard/Dashboard.tsx
@@ -6,7 +6,7 @@ const DashboardPage = () => {
     <div className="h-full w-full max-w-[1950px] m-auto">
       <div className="flex flex-1 h-full justify-between overflow-scroll">
         <Sidebar />
-        <div className="h-full w-full flex flex-col ">
+        <div className="h-full w-[100%] bg-[#f3f3f3] overflow-x-hidden overflow-y-scroll">
           <Outlet />
         </div>
       </div>
