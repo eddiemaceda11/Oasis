3697415b948dda0aa2931853ea996002e2e1a7c2
Eddie Maceda <eddiemaceda11@gmail.com>
2024-09-08 17:11:01 -0400

    Create the initialState and reducer for homeSlice

--- src/features/Dashboard/Home/homeSlice.ts
+++ src/features/Dashboard/Home/homeSlice.ts
@@ -2,10 +2,21 @@ import { createSlice } from "@reduxjs/toolkit";
 // import { type RentalProps } from "@/data/dashboard-rental-properties";
 import { rentalProperties } from "@/data/dashboard-rental-properties";
 
+const initialState = {
+  rentalProperties,
+  search: "",
+};
+
 const homeSlice = createSlice({
   name: "home",
-  initialState: rentalProperties,
-  reducers: {},
+  initialState,
+  reducers: {
+    filterProperties: (state, action) => {
+      state.search = action.payload;
+    },
+  },
 });
+
+export const { filterProperties } = homeSlice.actions;
 
 export default homeSlice.reducer;
